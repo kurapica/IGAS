b624fc4769001e1d3f615343349c93fa95b5113b
kurapica <kurapica.igas@gmail.com>
2013-02-06 02:53:30 +0800

    Fix scroll form

--- System/Widget/Common/ScrollForm.lua
+++ System/Widget/Common/ScrollForm.lua
@@ -31,6 +31,100 @@ class "ScrollForm"
 			@desc The container used for scroll form
 		]======]
 
+		local function ConvertPos(point)
+			if strfind(strupper(point), "BOTTOM") then
+				return 1
+			elseif strfind(strupper(point), "TOP") then
+				return -1
+			else
+				return 0
+			end
+		end
+
+		local function GetPos(frame, _Frame, _Center)
+			local point, relativeTo, relativePoint, xOfs, yOfs
+			local dx, dxp
+
+			_Center[frame] = _Center[frame] or 0
+
+			for i = 1, frame:GetNumPoints() do
+				point, relativeTo, relativePoint, xOfs, yOfs = frame:GetPoint(i)
+
+				if _Frame[relativeTo] then
+					if not _Center[relativeTo] then
+						GetPos(relativeTo, _Frame, _Center)
+					end
+
+					dx = ConvertPos(point)
+					dxp = ConvertPos(relativePoint)
+
+					if dx == -1 or _Center[frame] == 0 then
+						_Center[frame] = _Center[relativeTo] + dxp * (relativeTo.Height / 2) - yOfs - dx * (frame.Height / 2)
+					end
+				end
+			end
+		end
+
+		local function DoFixHeight(self)
+			local _Frame = {}
+			local _Center = {}
+			local _MaxHeight = 0
+			local h
+
+			_Frame[self] = true
+			_Center[self] = self.Height / 2
+
+			for i, v in pairs(self:GetChilds()) do
+				if v.GetPoint and v.Visible then
+					_Frame[v] = true
+				end
+			end
+
+			for i in pairs(_Frame) do
+				if not _Center[i] then
+					GetPos(i, _Frame, _Center)
+				end
+			end
+
+			for i, v in pairs(_Center) do
+				if i ~= self then
+					h = v + i.Height / 2
+					_MaxHeight = (_MaxHeight > h and _MaxHeight) or h
+				end
+			end
+
+			self.Height = _MaxHeight
+		end
+
+		local function FixScroll(self)
+			self:GetChild("Container").OnSizeChanged = self:GetChild("Container").OnSizeChanged - OnSizeChanged
+
+			if self.__AutoHeight then
+				DoFixHeight(self:GetChild("Container"))
+			end
+
+			local bar = self:GetChild("Bar")
+			local viewheight, height = self.Height, self:GetChild("Container").Height
+			local curvalue = bar.Value
+
+			if viewheight >= height then
+				if self.__AutoHeight then
+					self:GetChild("Container").Height = viewheight
+				end
+				bar:SetValue(0)
+	            bar:Disable()
+				bar:Hide()
+			else
+				local maxValue = height - viewheight
+	            if curvalue > maxValue then curvalue = maxValue end
+	            bar:SetMinMaxValues(0,maxValue)
+				bar:SetValue(curvalue)
+	            bar:Enable()
+				bar:Show()
+			end
+
+			self:GetChild("Container").OnSizeChanged = self:GetChild("Container").OnSizeChanged + OnSizeChanged
+		end
 		------------------------------------------------------
 		-- Script
 		------------------------------------------------------
@@ -38,6 +132,15 @@ class "ScrollForm"
 		------------------------------------------------------
 		-- Method
 		------------------------------------------------------
+		doc [======[
+			@name UpdateSize
+			@type method
+			@desc Update the size based on child elements
+			@return nil
+		]======]
+		function UpdateSize(self)
+
+		end
 
 		------------------------------------------------------
 		-- Property
@@ -176,6 +279,25 @@ class "ScrollForm"
 		bar:SetMinMaxValues(0, yrange)
 		bar:SetValue(value)
 	end
+
+    local function OnMouseWheel(self, delta)
+        local scrollBar = self:GetChild("Bar")
+		local minV, maxV = scrollBar:GetMinMaxValues()
+
+        if (delta > 0 ) then
+			if scrollBar.Value - (scrollBar.Height / 2) >= minV then
+				scrollBar.Value = scrollBar.Value - (scrollBar.Height / 2)
+			else
+				scrollBar.Value = minV
+			end
+        else
+			if scrollBar.Value + (scrollBar.Height / 2) <= maxV then
+				scrollBar.Value = scrollBar.Value + (scrollBar.Height / 2)
+			else
+				scrollBar.Value = maxV
+			end
+        end
+    end
 
 	------------------------------------------------------
 	-- Dispose
