89a79bd233e5b24f69403140ab97608145c74138
kurapica <kurapica125@outlook.com>
2020-10-17 10:30:08 +0800

    v165, russion localization added

---
+++ Localization/localization.ru.lua
@@ -0,0 +1,19 @@
+-- Thanks to https://github.com/Hubbotu
+local L = IGAS:NewLocale("IGAS", "ruRU")
+if not L then return end
+
+L["Okay"] = "Да"
+L["No"] = "Нет"
+L["Cancel"] = "Выбор"
+L["Color Picker"] = "Палитра цветов"
+
+L["Please input the search content"] = "Пожалуйста, введите содержание поиска"
+L["Please input the line number"] = "Пожалуйста, введите номер строки"
+L["'%s' can't be found in the content."] = "'%s' не может быть найдено в содержании."
+
+L["[%s][%s]'s binding key[%s] is removed in group[%s]"] = "[%s][%s]'s ключ привязки[%s] удален в группе[%s]"
+L["[%s]'s binding key[%s] is removed in group[%s]"] = "[%s]'s ключ привязки[%s] удален в группе[%s]"
+
+L["Move cursor over the button to binding key."] = "Наведите курсор на кнопку для привязки ключа."
+
+L["Click the child's anchor point and then click the parent's to bind them, or double-click the anchor point to unbind them."] = "Щелкните точку привязки дочернего элемента, а затем нажмите родительскую точку, чтобы связать их, или дважды щелкните точку привязки, чтобы отсоединить их."
